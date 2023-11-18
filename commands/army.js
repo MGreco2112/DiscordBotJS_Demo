df5d68f65b35617480a4490b5f530edbd0347fd0
Matthew Greco <mgreco2112@tutanota.com>
2023-11-18 13:50:45 -0500

    uitilized Clyde to finish writing army.js

--- commands/army.js
+++ commands/army.js
@@ -1,13 +1,13 @@
-// import { SlashCommandBuilder ,EmbedBuilder, AttachmentBuilder } from "discord.js";
+//CODE FROM DISCORD'S AI CLYDE
+import { SlashCommandBuilder } from 'discord.js';
 
-// export const data = new SlashCommandBuilder().setName("army").setDescription("Deploy Army");
+export const data = new SlashCommandBuilder()
+    .setName('army')
+    .setDescription('Deploy Army');
 
-// export async function execute(interaction) {
-//     const channel = Message.channel;
-
-//     const file = new AttachmentBuilder("../assets/droid_army.avif");
-
-//     const armyEmbed = new EmbedBuilder().setTitle("Army").setImage("attachment://droid_army.avif");
-
-//     await channel.send({ embeds: [armyEmbed], files:[file]});
-// }
+    
+export async function execute(interaction) {
+    await interaction.reply('Posting an image...');
+    const channel = interaction.channel;
+    await channel.send({ files: ["https://lumiere-a.akamaihd.net/v1/images/battledroids_d036e015.jpeg?region=0%2C0%2C800%2C600"] });
+}
