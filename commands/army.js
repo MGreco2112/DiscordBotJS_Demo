f81a5a276b5f8dd7e60a84569e036267897ed775
Matthew Greco <mgreco2112@tutanota.com>
2023-11-18 13:51:37 -0500

    updated the posting message when deploying an army

--- commands/army.js
+++ commands/army.js
@@ -7,7 +7,7 @@ export const data = new SlashCommandBuilder()
 
     
 export async function execute(interaction) {
-    await interaction.reply('Posting an image...');
+    await interaction.reply('Deploying Army!');
     const channel = interaction.channel;
     await channel.send({ files: ["https://lumiere-a.akamaihd.net/v1/images/battledroids_d036e015.jpeg?region=0%2C0%2C800%2C600"] });
 }
